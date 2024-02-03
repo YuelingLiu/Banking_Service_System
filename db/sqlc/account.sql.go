664f8371f5af5252971a3b6ca9258dc94d1b409a
Yueling Liu <stellaliuca@gmail.com>
2024-02-03 11:47:14 -0800

    account test cases passed

--- db/sqlc/account.sql.go
+++ db/sqlc/account.sql.go
@@ -91,7 +91,7 @@ func (q *Queries) GetAccount(ctx context.Context, id int64) (Account, error) {
 	return i, err
 }
 
-const updateAccount = `-- name: UpdateAccount :exec
+const updateAccount = `-- name: UpdateAccount :one
 UPDATE account
 SET balance = $2
 WHERE id = $1
@@ -103,9 +103,17 @@ type UpdateAccountParams struct {
 	Balance int64 `json:"balance"`
 }
 
-func (q *Queries) UpdateAccount(ctx context.Context, arg UpdateAccountParams) error {
-	_, err := q.db.ExecContext(ctx, updateAccount, arg.ID, arg.Balance)
-	return err
+func (q *Queries) UpdateAccount(ctx context.Context, arg UpdateAccountParams) (Account, error) {
+	row := q.db.QueryRowContext(ctx, updateAccount, arg.ID, arg.Balance)
+	var i Account
+	err := row.Scan(
+		&i.ID,
+		&i.Owner,
+		&i.Balance,
+		&i.Currency,
+		&i.CreatedAt,
+	)
+	return i, err
 }
 
 const listAccounts = `-- name: listAccounts :many
